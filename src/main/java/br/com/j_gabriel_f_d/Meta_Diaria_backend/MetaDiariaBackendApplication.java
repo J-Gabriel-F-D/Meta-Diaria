99c1f7cdf46084596ddb2f12d7865835f38b8dff
José Gabriel <jgabrielfd0@gmail.com>
2025-11-25 10:11:13 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/br/com/j_gabriel_f_d/Meta_Diaria_backend/MetaDiariaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.j_gabriel_f_d.Meta_Diaria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaDiariaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaDiariaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/j_gabriel_f_d/Meta_Diaria_backend/MetaDiariaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.j_gabriel_f_d.Meta_Diaria_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaDiariaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
